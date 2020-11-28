5df6e85b33680b5143892730ae2b1eafd536aaee
chosangwon93 <66065293+chosangwon93@users.noreply.github.com>
2020-11-28 18:27:25 +0900

    Update 05_gen-audit-config.sh

--- manifest/05_gen-audit-config.sh
+++ manifest/05_gen-audit-config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-CURRENT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-export HYPERCLOUD4_CA_CERT=$(openssl base64 -A <"$CURRENT_PATH/../pki/ca.crt")
+export HYPERCLOUD4_CA_CERT=$(openssl base64 -A <"${HPCD_HOME}/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/pki/ca.crt")
 
 AUDIT_CONFIG_FILE=06_audit-webhook-config
 if [ -f "$AUDIT_CONFIG_FILE" ]; then
@@ -9,4 +8,4 @@ if [ -f "$AUDIT_CONFIG_FILE" ]; then
 fi
 
 echo "Generate audit config file."
-envsubst < ./"$AUDIT_CONFIG_FILE".template  > "$AUDIT_CONFIG_FILE"
+envsubst < ${HPCD_HOME}/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/"$AUDIT_CONFIG_FILE".template  > "$AUDIT_CONFIG_FILE"
