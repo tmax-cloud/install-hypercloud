c23d0f813aa2c9c94dfaea503e9937f222352f2c
chosangwon93 <66065293+chosangwon93@users.noreply.github.com>
2020-11-28 18:26:56 +0900

    Update 03_gen-webhook-config.sh

--- manifest/03_gen-webhook-config.sh
+++ manifest/03_gen-webhook-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CURRENT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-export HYPERCLOUD4_CA_CERT=$(openssl base64 -A <"$CURRENT_PATH/../pki/ca.crt")
+
+export HYPERCLOUD4_CA_CERT=$(openssl base64 -A <"${HPCD_HOME}/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/pki/ca.crt")
 
 WEBHOOK_CONFIG_FILE=04_webhook-configuration.yaml
 if [ -f "$WEBHOOK_CONFIG_FILE" ]; then
@@ -9,4 +9,4 @@ if [ -f "$WEBHOOK_CONFIG_FILE" ]; then
 fi
 
 echo "Generate webhook config file."
-envsubst < ./"$WEBHOOK_CONFIG_FILE".template  > "$WEBHOOK_CONFIG_FILE"
+envsubst < ${HPCD_HOME}/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/"$WEBHOOK_CONFIG_FILE".template  > "$WEBHOOK_CONFIG_FILE"
