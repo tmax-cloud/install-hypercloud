85ebf266375dd4739f3144528677c7a9bf8f9470
cho <sangwon9377@gmail.com>
2021-01-27 11:29:09 +0900

    [mod] mod file path

--- manifest/hypercloud-webhook/02_gen-webhook-config.sh
+++ manifest/hypercloud-webhook/02_gen-webhook-config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export HYPERCLOUD4_CA_CERT=$(openssl base64 -A <"${HPCD_HOME}/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/pki/ca.crt")
+export HYPERCLOUD4_CA_CERT=$(openssl base64 -A <"${HPCD_HOME}/manifest/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/pki/ca.crt")
 
 WEBHOOK_CONFIG_FILE=03_webhook-configuration.yaml
-if [ -f "$WEBHOOK_CONFIG_FILE" ]; then
+if [ -f ${HPCD_HOME}/manifest/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/"$WEBHOOK_CONFIG_FILE" ]; then
    echo "Remove existed webhook config file."
-   rm $WEBHOOK_CONFIG_FILE
+   rm ${HPCD_HOME}/manifest/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/$WEBHOOK_CONFIG_FILE
 fi
 
 echo "Generate webhook config file."
-envsubst < ${HPCD_HOME}/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/"$WEBHOOK_CONFIG_FILE".template  > "$WEBHOOK_CONFIG_FILE"
+envsubst < ${HPCD_HOME}/manifest/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/"$WEBHOOK_CONFIG_FILE".template  > ${HPCD_HOME}/manifest/hypercloud-webhook-${HPCD_WEBHOOK_VERSION}/"$WEBHOOK_CONFIG_FILE"
