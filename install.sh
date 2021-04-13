9af7003c65f9dc57202f3b45480a26965ce2038f
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-04-13 17:06:16 +0900

    sudo 권한 추가, limits 추가

--- install.sh
+++ install.sh
@@ -11,16 +11,16 @@ set -xe
 
 
 # Check if certmanager exists
-if [ -z "$(kubectl get ns | grep cert-manager | awk '{print $1}')" ]; then
-  kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
-  timeout 5m kubectl -n cert-manager rollout status deployment/cert-manager
-  timeout 5m kubectl -n cert-manager rollout status deployment/cert-manager-cainjector
-  timeout 5m kubectl -n cert-manager rollout status deployment/cert-manager-webhook
+if [ -z "$(sudo kubectl get ns | grep cert-manager | awk '{print $1}')" ]; then
+  sudo kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
+  sudo timeout 5m kubectl -n cert-manager rollout status deployment/cert-manager
+  sudo timeout 5m kubectl -n cert-manager rollout status deployment/cert-manager-cainjector
+  sudo timeout 5m kubectl -n cert-manager rollout status deployment/cert-manager-webhook
 fi
 
 # Check if namespace exists
-if [ -z "$(kubectl get ns | grep hypercloud5-system | awk '{print $1}')" ]; then
-   kubectl create ns hypercloud5-system
+if [ -z "$(sudo kubectl get ns | grep hypercloud5-system | awk '{print $1}')" ]; then
+   sudo kubectl create ns hypercloud5-system
 fi
 
 # Install pkg or binary
@@ -30,28 +30,28 @@ if ! command -v sshpass 2>/dev/null ; then
 fi
 
 if ! command -v yq 2>/dev/null ; then
-  wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -O /usr/bin/yq &&\
-  chmod +x /usr/bin/yq
+  sudo wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -O /usr/bin/yq &&\
+  sudo chmod +x /usr/bin/yq
 fi
 
 # Install pkg or binary
 if ! command -v kustomize 2>/dev/null ; then
-  curl -L -O "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
-  tar -xzvf "kustomize_${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
-  chmod +x kustomize
+  sudo curl -L -O "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
+  sudo tar -xzvf "kustomize_${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
+  sudo chmod +x kustomize
   sudo mv kustomize /usr/local/bin/.
-  rm "kustomize_${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
+  sudo rm "kustomize_${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
 fi
 
 # Install hypercloud-single-server
 pushd $HYPERCLOUD_SINGLE_OPERATOR_HOME
-  kubectl apply -f  hypercloud-single-operator-v${HPCD_SINGLE_OPERATOR_VERSION}.yaml
+  sudo kubectl apply -f  hypercloud-single-operator-v${HPCD_SINGLE_OPERATOR_VERSION}.yaml
 popd
 
 #Install hypercloud-multi-server
 if [ $HPCD_MODE == "multi" ]; then
   pushd $HYPERCLOUD_MULTI_OPERATOR_HOME
-    kubectl apply -f  hypercloud-multi-operator-v${HPCD_MULTI_OPERATOR_VERSION}.yaml
+    sudo kubectl apply -f  hypercloud-multi-operator-v${HPCD_MULTI_OPERATOR_VERSION}.yaml
   popd
 fi
 
@@ -59,91 +59,91 @@ fi
 # step 1  - create pki and secret
 if [ ! -f "$HYPERCLOUD_API_SERVER_HOME/pki/hypercloud-api-server.crt" ] || [ ! -f "$HYPERCLOUD_API_SERVER_HOME/pki/hypercloud-api-server.key" ]; then
 pushd $HYPERCLOUD_API_SERVER_HOME/pki
-  chmod +x *.sh
+  sudo chmod +x *.sh
   sudo ./generateTls.sh -name=hypercloud-api-server -dns=hypercloud5-api-server-service.hypercloud5-system.svc -dns=hypercloud5-api-server-service.hypercloud5-system.svc.cluster.local
-  if [ -z "$(kubectl get secret hypercloud5-api-server-certs -n hypercloud5-system | awk '{print $1}')" ]; then
-    kubectl -n hypercloud5-system create secret generic hypercloud5-api-server-certs \
+  if [ -z "$(sudo kubectl get secret hypercloud5-api-server-certs -n hypercloud5-system | awk '{print $1}')" ]; then
+    sudo kubectl -n hypercloud5-system create secret generic hypercloud5-api-server-certs \
     --from-file=$HYPERCLOUD_API_SERVER_HOME/pki/hypercloud-api-server.crt \
     --from-file=$HYPERCLOUD_API_SERVER_HOME/pki/hypercloud-api-server.key
   else
-    kubectl -n hypercloud5-system delete secret  hypercloud5-api-server-certs
-    kubectl -n hypercloud5-system create secret generic hypercloud5-api-server-certs \
+    sudo kubectl -n hypercloud5-system delete secret  hypercloud5-api-server-certs
+    sudo kubectl -n hypercloud5-system create secret generic hypercloud5-api-server-certs \
     --from-file=$HYPERCLOUD_API_SERVER_HOME/pki/hypercloud-api-server.crt \
     --from-file=$HYPERCLOUD_API_SERVER_HOME/pki/hypercloud-api-server.key
   fi
 popd
 fi
 
-if [ -z "$(kubectl get cm -n hypercloud5-system | grep html-config | awk '{print $1}')" ]; then
-  kubectl create configmap html-config --from-file=$HYPERCLOUD_API_SERVER_HOME/html/invite.html -n hypercloud5-system
+if [ -z "$(sudo kubectl get cm -n hypercloud5-system | grep html-config | awk '{print $1}')" ]; then
+  sudo kubectl create configmap html-config --from-file=$HYPERCLOUD_API_SERVER_HOME/html/invite.html -n hypercloud5-system
 fi
 
 
 # step 2  - sed manifests
 if [ $REGISTRY != "{REGISTRY}" ]; then
-  sed -i 's#tmaxcloudck/hypercloud-api-server#'${REGISTRY}'/hypercloud-api-server#g' ${HYPERCLOUD_API_SERVER_HOME}/03_hypercloud-api-server.yaml
-  sed -i 's#tmaxcloudck/postgres-cron#'${REGISTRY}'/postgres-cron#g' ${HYPERCLOUD_API_SERVER_HOME}/02_postgres-create.yaml
+  sudo sed -i 's#tmaxcloudck/hypercloud-api-server#'${REGISTRY}'/hypercloud-api-server#g' ${HYPERCLOUD_API_SERVER_HOME}/03_hypercloud-api-server.yaml
+  sudo sed -i 's#tmaxcloudck/postgres-cron#'${REGISTRY}'/postgres-cron#g' ${HYPERCLOUD_API_SERVER_HOME}/02_postgres-create.yaml
 fi
-sed -i 's/{HPCD_API_SERVER_VERSION}/b'${HPCD_API_SERVER_VERSION}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/03_hypercloud-api-server.yaml
-sed -i 's/{HPCD_MODE}/'${HPCD_MODE}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/03_hypercloud-api-server.yaml
-sed -i 's/{HPCD_POSTGRES_VERSION}/b'${HPCD_POSTGRES_VERSION}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/02_postgres-create.yaml
-sed -i 's/{INVITATION_TOKEN_EXPIRED_DATE}/'${INVITATION_TOKEN_EXPIRED_DATE}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/02_postgres-create.yaml
-sed -i 's/{INVITATION_TOKEN_EXPIRED_DATE}/'${INVITATION_TOKEN_EXPIRED_DATE}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/03_hypercloud-api-server.yaml
+sudo sed -i 's/{HPCD_API_SERVER_VERSION}/b'${HPCD_API_SERVER_VERSION}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/03_hypercloud-api-server.yaml
+sudo sed -i 's/{HPCD_MODE}/'${HPCD_MODE}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/03_hypercloud-api-server.yaml
+sudo sed -i 's/{HPCD_POSTGRES_VERSION}/b'${HPCD_POSTGRES_VERSION}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/02_postgres-create.yaml
+sudo sed -i 's/{INVITATION_TOKEN_EXPIRED_DATE}/'${INVITATION_TOKEN_EXPIRED_DATE}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/02_postgres-create.yaml
+sudo sed -i 's/{INVITATION_TOKEN_EXPIRED_DATE}/'${INVITATION_TOKEN_EXPIRED_DATE}'/g'  ${HYPERCLOUD_API_SERVER_HOME}/03_hypercloud-api-server.yaml
 
 
 # step 3  - apply manifests
 pushd $HYPERCLOUD_API_SERVER_HOME
-  kubectl apply -f  01_init.yaml
-  kubectl apply -f  02_postgres-create.yaml
-  kubectl apply -f  03_hypercloud-api-server.yaml
-  kubectl apply -f  04_default-role.yaml
+  sudo kubectl apply -f  01_init.yaml
+  sudo kubectl apply -f  02_postgres-create.yaml
+  sudo kubectl apply -f  03_hypercloud-api-server.yaml
+  sudo kubectl apply -f  04_default-role.yaml
 popd
 
 #  step 4 - create and apply config
 pushd $HYPERCLOUD_API_SERVER_HOME/config
-  chmod +x *.sh 
+  sudo chmod +x *.sh 
   sudo ./gen-audit-config.sh
   sudo ./gen-webhook-config.sh
   sudo cp audit-policy.yaml /etc/kubernetes/pki/
   sudo cp audit-webhook-config /etc/kubernetes/pki/
 
-  kubectl apply -f webhook-configuration.yaml
+  sudo kubectl apply -f webhook-configuration.yaml
 popd
 #  step 5 - modify kubernetes api-server manifest
 sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml .
-yq e '.spec.containers[0].command += "--audit-webhook-mode=batch"' -i ./kube-apiserver.yaml
-yq e '.spec.containers[0].command += "--audit-policy-file=/etc/kubernetes/pki/audit-policy.yaml"' -i ./kube-apiserver.yaml
-yq e '.spec.containers[0].command += "--audit-webhook-config-file=/etc/kubernetes/pki/audit-webhook-config"' -i ./kube-apiserver.yaml
-yq e 'del(.spec.dnsPolicy)' -i kube-apiserver.yaml
-yq e '.spec.dnsPolicy += "ClusterFirstWithHostNet"' -i kube-apiserver.yaml
-mv -f ./kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+sudo yq e '.spec.containers[0].command += "--audit-webhook-mode=batch"' -i ./kube-apiserver.yaml
+sudo yq e '.spec.containers[0].command += "--audit-policy-file=/etc/kubernetes/pki/audit-policy.yaml"' -i ./kube-apiserver.yaml
+sudo yq e '.spec.containers[0].command += "--audit-webhook-config-file=/etc/kubernetes/pki/audit-webhook-config"' -i ./kube-apiserver.yaml
+sudo yq e 'del(.spec.dnsPolicy)' -i kube-apiserver.yaml
+sudo yq e '.spec.dnsPolicy += "ClusterFirstWithHostNet"' -i kube-apiserver.yaml
+sudo mv -f ./kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
 
 
 #  step 6 - copy audit config files to all k8s-apiserver and modify k8s apiserver manifest
-IFS=' ' read -r -a masters <<< $(kubectl get nodes --selector=node-role.kubernetes.io/master -o jsonpath='{$.items[*].status.addresses[?(@.type=="InternalIP")].address}')
+IFS=' ' read -r -a masters <<< $(sudo kubectl get nodes --selector=node-role.kubernetes.io/master -o jsonpath='{$.items[*].status.addresses[?(@.type=="InternalIP")].address}')
 for master in "${masters[@]}"
 do
   if [ $master == $MAIN_MASTER_IP ]; then
     continue
   fi
 
-  sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master"  sudo wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -O /usr/bin/yq
-  sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master"  sudo chmod +x /usr/bin/yq
+  sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master"  sudo wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -O /usr/bin/yq
+  sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master"  sudo chmod +x /usr/bin/yq
 
   sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" scp /etc/kubernetes/pki/audit-policy.yaml ${MASTER_NODE_ROOT_USER}@"$master":/etc/kubernetes/pki/audit-policy.yaml
   sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" scp /etc/kubernetes/pki/audit-webhook-config ${MASTER_NODE_ROOT_USER}@"$master":/etc/kubernetes/pki/audit-webhook-config
 
-  sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml .
-  sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" 'sudo yq e '"'"'.spec.containers[0].command += "--audit-webhook-mode=batch"'"'"' -i ./kube-apiserver.yaml'
-  sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" 'sudo yq e '"'"'.spec.containers[0].command += "--audit-policy-file=/etc/kubernetes/pki/audit-policy.yaml"'"'"' -i ./kube-apiserver.yaml'
-  sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" 'sudo yq e '"'"'.spec.containers[0].command += "--audit-webhook-config-file=/etc/kubernetes/pki/audit-webhook-config"'"'"' -i ./kube-apiserver.yaml'
-  sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" 'sudo yq e '"'"'.spec.dnsPolicy += "ClusterFirstWithHostNet"'"'"' -i ./kube-apiserver.yaml'
-  sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" sudo mv -f ./kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+  sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml .
+  sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" 'sudo yq e '"'"'.spec.containers[0].command += "--audit-webhook-mode=batch"'"'"' -i ./kube-apiserver.yaml'
+  sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" 'sudo yq e '"'"'.spec.containers[0].command += "--audit-policy-file=/etc/kubernetes/pki/audit-policy.yaml"'"'"' -i ./kube-apiserver.yaml'
+  sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" 'sudo yq e '"'"'.spec.containers[0].command += "--audit-webhook-config-file=/etc/kubernetes/pki/audit-webhook-config"'"'"' -i ./kube-apiserver.yaml'
+  sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" 'sudo yq e '"'"'.spec.dnsPolicy += "ClusterFirstWithHostNet"'"'"' -i ./kube-apiserver.yaml'
+  sudo sshpass -p "$MASTER_NODE_ROOT_PASSWORD" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER}@"$master" sudo mv -f ./kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
 done
 
 sleep 30s
 #  step 7 - check all master is ready
-IFS=' ' read -r -a nodes <<< $(kubectl get nodes --selector=node-role.kubernetes.io/master -o jsonpath='{$.items[*].status.conditions[-1].type}')
+IFS=' ' read -r -a nodes <<< $(sudo kubectl get nodes --selector=node-role.kubernetes.io/master -o jsonpath='{$.items[*].status.conditions[-1].type}')
 for (( i=0; i<8; i++ )); do
   j=0;
   for node in "${nodes[@]}"
