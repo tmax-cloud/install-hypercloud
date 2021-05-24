51eaac20b962d4e8350614751bbac77805ddbd32
Seungwon Lee <41567934+aldlfkahs@users.noreply.github.com>
2021-05-24 16:10:09 +0900

    01_init version-config sed 오류 수정

--- install.sh
+++ install.sh
@@ -148,12 +148,12 @@ sudo yq e '.spec.containers[0].command += "--audit-policy-file=/etc/kubernetes/p
 sudo yq e '.spec.containers[0].command += "--audit-webhook-config-file=/etc/kubernetes/pki/audit-webhook-config"' -i ./kube-apiserver.yaml
 sudo yq e 'del(.spec.dnsPolicy)' -i kube-apiserver.yaml
 sudo yq e '.spec.dnsPolicy += "ClusterFirstWithHostNet"' -i kube-apiserver.yaml
-sudo mv -f ./kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
 
 # get hyperauth URL from --oidc-issuer-url and sed to version.config
 KA_YAML=`sudo yq e '.spec.containers[0].command' ./kube-apiserver.yaml`
 HYPERAUTH_URL=`echo "${KA_YAML#*--oidc-issuer-url=}" | tr -d '\12' | cut -d '-' -f1`
 sudo sed -i 's@{HYPERAUTH_URL}@'${HYPERAUTH_URL}'@g'  ${HYPERCLOUD_API_SERVER_HOME}/01_init.yaml
+sudo mv -f ./kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
 
 #  step 6 - copy audit config files to all k8s-apiserver and modify k8s apiserver manifest
 i=0
